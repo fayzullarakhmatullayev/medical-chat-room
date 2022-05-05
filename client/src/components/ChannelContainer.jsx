04e0d3358d315f72c6031b0cc3954d9b0d591726
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-05-05 20:16:03 +0500

    updated

---
+++ client/src/components/ChannelContainer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Channel, useChatContext, MessageTeam } from "stream-chat-react";
+import { ChannelInner, CreateChannel, EditChannel } from "./";
+
+const ChanelContainer = ({
+  isCreating,
+  setIsCreating,
+  setIsEditing,
+  isEditing,
+  createType,
+}) => {
+  const { channel } = useChatContext();
+
+  if (isCreating) {
+    return (
+      <div className="channel__container">
+        <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+      </div>
+    );
+  }
+  if (isEditing) {
+    return (
+      <div className="channel__container">
+        <EditChannel setIsEditing={setIsEditing} />
+      </div>
+    );
+  }
+
+  const EmptyState = () => (
+    <div className="channel__container">
+      <p className="channel-empty__first">
+        This is the begining of your chat history.
+      </p>
+      <p className="channel-empty__second">
+        Send messages, attachments, links, emojis, and more!
+      </p>
+    </div>
+  );
+
+  return (
+    <div className="channel__container">
+      <Channel
+        EmptyStateIndicator={EmptyState}
+        Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />}
+      >
+        <ChannelInner setIsEditing={setIsEditing} />
+      </Channel>
+    </div>
+  );
+};
+
+export default ChanelContainer;
